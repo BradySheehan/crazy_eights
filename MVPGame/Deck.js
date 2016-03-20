269cbcef77dbd0611e895340645fdad7f8de27a0
Brady Sheehan <Bradya.sheehan@gmail.com>
2016-03-20 12:56:48 -0400

    Functions for seeding the deck

--- MVPGame/Deck.js
+++ MVPGame/Deck.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// Crazy Eights Game Develped by Mathew Sobocinski and Brady Sheehan
-
 /**
  * Deck of playing cards.
  */
@@ -11,12 +9,33 @@ function Deck() {
    */
   var suit = ["c", "d", "h", "s"];
   var value = ["2", "3", "4", "5", "6", "7", "8", "9", "10",
-      	       "j", "q", "k", "a"];
+               "j", "q", "k", "a"];
   this.list = new Array();
   for (var i=0; i<suit.length; i++) {
     for (var j=0; j<value.length; j++) {
       this.list.push(new Card(suit[i],value[j]));
     }
+  }
+  // Use a seeded pseudo-random number generator if
+  // a "seed" parameter with numeric value is present in the URL,
+  // otherwise use the built-in Math prng.
+  var params = window.location.search.split(/[?=&]/);
+  var ind = params.indexOf("seed");
+  // If we found a parameter named "seed", use its value as prng seed.
+  // (We look for an odd index because params[0] will be the empty string
+  // since the search string begins with '?', so parameter names will be
+  // at 1, 3, etc.)
+  if (ind>=0 && ind%2==1) {
+    var seed = Number(params[ind+1]);
+    if (seed) { 
+      this.prng = new xorshift(seed);
+    }
+    else {
+      this.prng = Math;
+    }
+  }
+  else {
+    this.prng = Math;
   }
 }
 Deck.prototype = {
@@ -25,7 +44,7 @@ Deck.prototype = {
    */
   shuffle: function () {
     for (var n=this.list.length; n>=2; n--) {
-      var r = Math.floor(Math.random()*n);
+      var r = Math.floor(this.prng.random()*n);
       // Swap cards at r and n-1
       var card = this.list[r];
       this.list[r] = this.list[n-1];
